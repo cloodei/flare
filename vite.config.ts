138ba8c8eb1c3123035677086b9139f54f5e1a66
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-06-27 18:34:09 +0700

    see i

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 import react from '@vitejs/plugin-react-swc'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
 })
